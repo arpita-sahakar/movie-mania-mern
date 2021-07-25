80cfb00217ef4f022d5fca2a8db71a4f50eee68d
Arpita Saha <arpita.sahaa@gmail.com>
2021-07-25 10:12:36 -0400

    added login ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from "./container/Home";
 import SignUp from "./container/SignUp";
+import Login from "./container/Login";
 
 function App() {
   return (
     <>
+      <div className="signup">
+        <Login />
+      </div>
       <div className="signup">
         <SignUp />
       </div>
