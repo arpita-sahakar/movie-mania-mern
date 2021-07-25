a0a786697233f3993645c497cef43a69f5133028
Arpita Saha <arpita.sahaa@gmail.com>
2021-07-25 12:38:58 -0400

    changed logo on displaymovie page

--- client/src/container/NavBar.jsx
+++ client/src/container/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import "./NavBar.css";
 import logo from "../images/logo1.png";
-import profile from "../images/profile.png";
+import profile from "../images/Title_MM_transparent.PNG";
+// import profile from "../images/profile.png";
 
 function NavBar() {
     return (
         <div className="nav">
-            <img className="NavLogo" src={logo} alt={"MovieFlix Logo"}/>
-            <img className="profileLogo" src={profile} alt={"My Profile"}/>
+            <img className="NavLogo" src={logo} alt={"MovieFlix Logo"} />
+            <img className="profileLogo" src={profile} alt={"My Profile"} />
         </div>
     )
 }
