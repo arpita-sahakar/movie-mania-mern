a4e0a267ac8702cc2439d3efff11214e11563397
Arpita Saha <arpita.sahaa@gmail.com>
2021-07-25 11:23:49 -0400

    deleted unwanted codes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,16 +1,12 @@
 import express from "express";
 import { login, signUp, allUsers } from "../controllers/userController.js";
-// import { test } from "../controllers/testCont.js";
+
 
 const router = express.Router();
 
 router.get("/", allUsers)
 router.post("/login", login);
 router.post("/signup", signUp);
-// router.get("/", (req, res) => {
-//     res.send("hello routes")
-// })
 
-// router.get("/abc", test)
 
 export default router;
